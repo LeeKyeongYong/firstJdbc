a97365273fe8409ff74a08a50f10f1e6d09df52f
leeky <sleekydz86@icloud.com>
2022-07-20 23:36:45 +0900

    깃반영

--- firstJdbc/src/com/study/firstSelect/FirstJava.java
+++ firstJdbc/src/com/study/firstSelect/FirstJava.java
@@ -2,6 +2,6 @@ package com.study.firstSelect;
 
 public class FirstJava {//jdbc연결 확인
 	public static void main(String[] args) {
-		System.out.println("Jdbc시작합니다!!");
+		System.out.println("Jdbc시작합니다!!!");
 	}
 }
